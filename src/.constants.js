9db4226f5bd6dab7766d3530a7aedd16fcbe9136
Parveen Bhadoo <bhadoo@pm.me>
2019-11-13 21:13:34 +0530

    make it better

--- src/.constants.js
+++ src/.constants.js
@@ -26,4 +26,4 @@ module.exports = Object.freeze({
       NAME: "@Bhadoo3_bot"
     }
 });
-// Heroku Account ID PASSWORD kavita@gtb.org.in katehra@1
+// Heroku Account ID PASSWORD varunswami.mba@gtb.org.in Bhadoo@1 it
